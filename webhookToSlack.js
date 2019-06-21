2681c5660968ae5e67d66391dc66edb4d3a90b3d
dposse <dposse.125@gmail.com>
2019-06-20 20:34:25 -0400

    Rename webhookToSlack, create slack url

---
+++ webhookToSlack.js
@@ -0,0 +1,30 @@
+/**
+ * Webhook module for Slack bot
+ * Author: Daniel Posse
+ */
+
+ /**
+  * [ ] get url from jorge's webhook
+  * [ ] create post request
+  * [ ] send post request to url
+  * [ ] customize bot appearance
+  * [ ] set bot name
+  */
+
+//!DON'T CHANGE CHANNEL BOT IS POSTED TO UNTIL TESTING IS COMPLETE!
+
+const SLACK_URL = 'https://hooks.slack.com/services/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';
+
+
+
+//async iife? like the cool kids do
+(async function() {
+
+  try {
+
+  }
+  catch(error) {
+    console.error('hey we got error: ', error);
+  }
+
+})();
