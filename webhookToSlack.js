d8eb24cdffffc9b0d1c70e57e2e7bc119fe41e7f
dposse <dposse.125@gmail.com>
2019-06-21 07:35:44 -0400

    Added request, got new webhook url

--- webhookToSlack.js
+++ webhookToSlack.js
@@ -4,7 +4,7 @@
  */
 
  /**
-  * [ ] get url from jorge's webhook
+  * [X] get url from ~jorge's~ new webhook
   * [ ] create post request
   * [ ] send post request to url
   * [ ] customize bot appearance
@@ -13,18 +13,23 @@
 
 //!DON'T CHANGE CHANNEL BOT IS POSTED TO UNTIL TESTING IS COMPLETE!
 
-const SLACK_URL = 'https://hooks.slack.com/services/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';
+const request = require('request-promise');
 
+const SLACK_URL = 'https://hooks.slack.com/services/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';
 
 
 //async iife? like the cool kids do
 (async function() {
-
   try {
+
+    //get data from main.js - maybe refactor this to be a called function in main
+    //for now hardcode test data
+
+    //send to webhook
+
 
   }
   catch(error) {
     console.error('hey we got error: ', error);
   }
-
 })();
