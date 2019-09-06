b30a74dae2acdcc50ca374cf20989903e180842d
snpdolan <snpdolan@gmail.com>
2019-09-06 15:34:52 -0400

    added redsox_games file for hacky check, and sketch of date function.

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 
 
 const webhookToSlack = require('./webhookToSlack');
+//grab yesterday's date string after getting today as a new Date object
+const yesterdaysDate = new Date();
+yesterdaysDate.setDate(yesterdaysDate.getDate() - 1)
+const yesterdayArray = yesterdaysDate.toDateString().split(' ');
+//convert to mm/dd/yy format
+const yesterdayFormatted = yesterdayArray.splice(1,3).join("/");
+//check the day against Boston games
+
+  //create path string if Boston had a game 
+
 
 // const Mlbgames = require('mlbgames');
 // const options = {
