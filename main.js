0300632f3efc1cfe679f1d9c26b1b6f25a9e911d
dposse <dposse.125@gmail.com>
2019-09-07 09:55:08 -0400

    Added notes to main and cypress script to package.json

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+/**
+ * General plan -
+ * Run node daily (heroku scheduler http://www.modeo.co/blog/2015/1/8/heroku-scheduler-with-nodejs-tutorial)
+ * GET MLB data
+ *  currently hard coded to red sox and yankees, make dynamic in the future
+ * Based on result, send slackbot message
+ */
 
 
 const webhookToSlack = require('./webhookToSlack');
@@ -9,7 +16,7 @@ const yesterdayArray = yesterdaysDate.toDateString().split(' ');
 const yesterdayFormatted = yesterdayArray.splice(1,3).join("/");
 //check the day against Boston games
 
-  //create path string if Boston had a game 
+//create path string if Boston had a game 
 
 const slackInfo = require('./config');
 
@@ -26,5 +33,7 @@ const slackInfo = require('./config');
 
 const slackbot = webhookToSlack();
 
+// red sox win, lose, tie
+// yankees win, lose, tie
 
-slackbot.sendMessage(slackInfo.SLACK_CHANNEL_URL, 'testing with user but no altmessage', slackInfo.MEMBER_ID);
+// slackbot.sendMessage(slackInfo.SLACK_CHANNEL_URL, 'testing with user but no altmessage', slackInfo.MEMBER_ID);
