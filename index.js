84a473d94d6be27eaf81ef17d5d0c15fd97d6ee9
dposse <dposse.125@gmail.com>
2020-04-04 15:25:13 -0400

    Fix issue with async in aws secret manager

--- index.js
+++ index.js
@@ -5,66 +5,42 @@
  *  currently hard coded to red sox and yankees, make dynamic in the future
  * Based on result, send slackbot message
  */
-// Load the AWS SDK
 const AWS = require('aws-sdk');
 const checkMlbGames = require('./services/mlb/mlbGames');
 const { MEMBER_ID, MY_TEAM } = require('./config');
 const request = require('request-promise');
 
+//for aws secrets
+const region = "us-east-1";
+const secretName = "bballSlackbot-slack-url";
+
+const client = new AWS.SecretsManager({ region: region });
+
+const getAwsSecret = (secretName) => {
+  return client.getSecretValue({ SecretId: secretName }).promise();
+}
+
+const getAwsSecretAsync = async (secretName) => {
+  let error;
+  const response = await getAwsSecret(secretName)
+    .catch(err => error = err);
+  return [error, response];
+}
+
 module.exports.runBaseballSlackbot = async () => {
   console.log(`in runBaseballSlackbot`);
-  // Following code is from aws secrets docs
-  const region = "us-east-1";
-  const secretName = "bballSlackbot-slack-url";
-  let secret;
-  //not used, but here as example
-  let decodedBinarySecret;
+  const [error, secretObj] = await getAwsSecretAsync(secretName);
 
-  // Create a Secrets Manager client
-  console.log(`before aws.secretsmanager`);
-  const client = new AWS.SecretsManager({ region: region });
-  console.log(`client:`, client);
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-  // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-  // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-  // We rethrow the exception by default.
-  client.getSecretValue({SecretId: secretName}, async function(err, data) {
-    console.log(err, data);
-    if (err) {
-      if (err.code === 'DecryptionFailureException')
-        // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-        // Deal with the exception here, and/or rethrow at your discretion.
-        throw err;
-      else if (err.code === 'InternalServiceErrorException')
-        // An error occurred on the server side.
-        // Deal with the exception here, and/or rethrow at your discretion.
-        throw err;
-      else if (err.code === 'InvalidParameterException')
-        // You provided an invalid value for a parameter.
-        // Deal with the exception here, and/or rethrow at your discretion.
-        throw err;
-      else if (err.code === 'InvalidRequestException')
-        // You provided a parameter value that is not valid for the current state of the resource.
-        // Deal with the exception here, and/or rethrow at your discretion.
-        throw err;
-      else if (err.code === 'ResourceNotFoundException')
-        // We can't find the resource that you asked for.
-        // Deal with the exception here, and/or rethrow at your discretion.
-        throw err;
-    }
-    else {
-      // Decrypts secret using the associated KMS CMK.
-      // Depending on whether the secret is a string or binary, one of these fields will be populated.
-      if ('SecretString' in data) {
-        secret = JSON.parse(data.SecretString);
-      } else {
-        let buff = new Buffer(data.SecretBinary, 'base64');
-        decodedBinarySecret = buff.toString('ascii');
-      }
-    }
-  });
+  console.log(secretObj);
 
-  console.log(secret);
+  // get slack url from within secret
+  const slackUrl = JSON.parse(secretObj.SecretString).SLACK_CHANNEL_URL;
+
   //checkMLbGames defaults to using NY Yankees code
   const gameResultMessage = MY_TEAM === '' ? 
     await checkMlbGames() :
@@ -76,7 +52,7 @@ module.exports.runBaseballSlackbot = async () => {
 
     //send message to slack
     const response = await request({
-      url: secret.SLACK_CHANNEL_URL,
+      url: slackUrl,
       method: 'POST',
       body: {
         text: messageWithTarget
